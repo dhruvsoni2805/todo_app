5a6a7165e87a18a1b2443d57e5882d479322b711
Spidy-29 <spidy.techbuddy@gmail.com>
2023-06-03 20:10:30 +0530

    Implemented ToDo App

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+import '../model/todo.dart';
+
+class ToDoItem extends StatelessWidget {
+  final ToDo todo;
+  final onToDoChanged;
+  final onDeleteItem;
+  const ToDoItem({
+    super.key,
+    required this.todo,
+    required this.onDeleteItem,
+    required this.onToDoChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        onTap: () {
+          onToDoChanged(todo);
+        },
+        contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        tileColor: Colors.white,
+        leading: Icon(
+          todo.isDone ? Icons.check_box : Icons.check_box_outline_blank,
+          color: tdBlue,
+        ),
+        title: Text(
+          todo.todoText!,
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: todo.isDone ? TextDecoration.lineThrough : null,
+          ),
+        ),
+        trailing: Container(
+          padding: const EdgeInsets.all(0),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            icon: const Icon(Icons.delete),
+            color: Colors.white,
+            iconSize: 18,
+            onPressed: () {
+              onDeleteItem(todo.id);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
